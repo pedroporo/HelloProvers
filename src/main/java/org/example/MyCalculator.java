fa18c948fbf66d44555777f4ac66adb868784351
pedroporo <pedroguillferri9@gmail.com>
2024-05-29 20:08:12 +0200

    Parte D lanzamiento de errores para todas las operaciones si tienen un numero negativo

--- src/main/java/org/example/MyCalculator.java
+++ src/main/java/org/example/MyCalculator.java
@@ -3,15 +3,27 @@ package org.example;
 public class MyCalculator {
 
     public int add(int a,int b) {
+        if (a<0 || b<0){
+            throw new IllegalArgumentException("La suma no puede contener numeros negativos");
+        }
         return a+b;
     }
     public int sub(int a,int b) {
+        if (a<0 || b<0){
+            throw new IllegalArgumentException("La resta no puede contener numeros negativos");
+        }
         return a-b;
     }
     public int mult(int a,int b) {
+        if (a<0 || b<0){
+            throw new IllegalArgumentException("La multiplicacion no puede contener numeros negativos");
+        }
         return a*b;
     }
     public int div(int a,int b) {
+        if (a<0 || b<0){
+            throw new IllegalArgumentException("La division no puede contener numeros negativos");
+        }
         if (b==0){
             throw new ArithmeticException("Division Por Cero");
         }
--- src/test/java/org/example/MyCalculatorTest.java
+++ src/test/java/org/example/MyCalculatorTest.java
@@ -23,34 +23,79 @@ class MyCalculatorTest {
     void add() {
 
         int expectedResult=10;
-        int actualResult=myCalculator.add(5,5);
+
+        int num1=5;
+        int num2=5;
+
+        int actualResult=myCalculator.add(num1,num2);
         assertEquals(expectedResult,actualResult);
     }
 
     @Test
     void sub() {
         int expectedResult=2;
-        int actualResult=myCalculator.sub(7,5);
+        int num1=7;
+        int num2=5;
+        int actualResult=myCalculator.sub(num1,num2);
         assertEquals(expectedResult,actualResult);
     }
 
     @Test
     void mult() {
         int expectedResult=25;
-        int actualResult=myCalculator.mult(5,5);
+        int num1=5;
+        int num2=5;
+        int actualResult=myCalculator.mult(num1,num2);
         assertEquals(expectedResult,actualResult);
     }
 
     @Test
     void div() {
         int expectedResult=5;
-        int actualResult=myCalculator.div(25,5);
+        int num1=25;
+        int num2=5;
+        int actualResult=myCalculator.div(num1,num2);
         assertEquals(expectedResult,actualResult);
     }
     @Test
     void divByZero() {
         int expectedResult=5;
-        int actualResult=myCalculator.div(25,0);
+        int num1=5;
+        int num2=0;
+        int actualResult=myCalculator.div(num1,num2);
         assertEquals(expectedResult,actualResult);
+    }
+    @Test
+    void addWhenNegativeThrowsException(){
+
+        int expectedResult=10;
+
+        int num1=-5;
+        int num2=2;
+
+
+        assertThrows(IllegalArgumentException.class, () ->myCalculator.add(num1,num2));
+    }
+    @Test
+    void subWhenNegativeThrowsException(){
+        int expectedResult=10;
+
+        int num1=-7;
+        int num2=5;
+
+        assertThrows(IllegalArgumentException.class, () ->myCalculator.sub(num1,num2));
+    }
+
+    @Test
+    void multWhenNegativeThrowsException(){
+        int num1=5;
+        int num2=-5;
+        assertThrows(IllegalArgumentException.class, () ->myCalculator.mult(num1,num2));
+    }
+    @Test
+    void divWhenNegativeThrowsException(){
+        int num1=-25;
+        int num2=3;
+        assertThrows(IllegalArgumentException.class, () ->myCalculator.div(num1,num2));
     }
 }
